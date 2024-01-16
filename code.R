64a4413607e4dc204f9adcc310b365f04f7819ae
je-blueink <je.blueink@gmail.com>
2024-01-17 00:30:20 +0900

    pre-processing, extracting top 100 words of first book

--- code.R
+++ code.R
@@ -35,3 +35,31 @@ crawl_books <- function(dataframe) {
 }
 
 book_lists <- crawl_books(top30)
+
+# 본문 추출
+book1_chpt <- grep(book_lists[[1]], pattern = "CHAPTER")
+book1_end <- grep(book_lists[[1]], pattern = "END OF THE PROJECT GUTENBERG EBOOK")-2
+book_lists[[1]][562]
+book_lists[[1]][19048]
+book1_body <- book_lists[[1]][(book1_chpt[136]):book1_end] 
+
+# 전처리
+book1_body %>% paste(collapse = " ") %>%
+  tolower() %>%
+  gsub(pattern = "’s", replacement="") %>%
+  gsub(pattern = "([^[:alnum:][:blank:]’-])", replacement = "")
+
+#토큰화
+book1_token <- unlist(strsplit(book1_body, " "))
+
+#원형복원 
+book1_lemma <- lemmatize_strings(book1_token) 
+
+#불용어 삭제(단어 벡터)
+book1_words <- book1_lemma[!book1_lemma %in% c(stopwords(), "")] 
+
+#단어 도수분포표 만들기
+book1_table <- sort(table(book1_words), decreasing = T) 
+
+#최빈출 단어 100개 추출
+book1_topwords <- book1_table %>% head(100) %>% names() %>% as.vector()
